df8604653267bf2a633491ae15aa2f41f1c3b19c
andressonjaniodias-blip <andressonjaniodias@gmail.com>
2026-07-11 02:21:18 -0300

    Delete theme-switcher.js

--- frontend/assets/js/theme-switcher.js
+++
@@ -1,5 +0,0 @@
-/* Aplica o tema salvo imediatamente para evitar flash */
-(function () {
-  const tema = localStorage.getItem('gcm-tema') || 'gov-modern';
-  document.documentElement.setAttribute('data-theme', tema);
-})();
